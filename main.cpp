508b36aa085d6b375d24f25ac481a22a48fee23e
mauricio <mss.soares@hotmail.com>
2019-02-25 19:30:52 -0300

    labirynth draw and colors menu

--- main.cpp
+++ main.cpp
@@ -34,8 +34,6 @@ void Draw(void){
 		glVertex3f(pX, pY+2, 0);
 	glEnd();
 	
-//	glClear(GL_COLOR_BUFFER_BIT);
-	
 	glColor3f(lR,lG,lB);
 	glBegin(GL_POLYGON);
 		glVertex3f(48, 19, 0);
@@ -45,10 +43,10 @@ void Draw(void){
 	glEnd();
 	
 	glBegin(GL_POLYGON);
-		glVertex3f(32, 39, 0);
+		glVertex3f(47, 39, 0);
 		glVertex3f(72, 39, 0);
 		glVertex3f(72, 40, 0);
-		glVertex3f(32, 40, 0);
+		glVertex3f(47, 40, 0);
 	glEnd();
 	
 	glBegin(GL_POLYGON);
@@ -70,6 +68,49 @@ void Draw(void){
 		glVertex3f(75, 35, 0);
 		glVertex3f(75, 43, 0);		
 		glVertex3f(76, 43, 0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(76, 43, 0);
+		glVertex3f(54, 43, 0);
+		glVertex3f(54, 44, 0);		
+		glVertex3f(76, 44, 0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(50, 40, 0);
+		glVertex3f(51, 40, 0);
+		glVertex3f(51, 47, 0);		
+		glVertex3f(50, 47, 0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(50, 47, 0);
+		glVertex3f(50, 48, 0);
+		glVertex3f(57, 48, 0);		
+		glVertex3f(57, 47, 0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(60, 44, 0);
+		glVertex3f(60, 54, 0);
+		glVertex3f(61, 54, 0);		
+		glVertex3f(61, 44, 0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(57, 47, 0);
+		glVertex3f(56, 47, 0);
+		glVertex3f(56, 54, 0);		
+		glVertex3f(57, 54, 0);
+	glEnd();
+	
+	glColor3f(lR+0.3,lG,lB+0.1);
+	glBegin(GL_POLYGON);
+		glVertex3f(61, 54, 0);
+		glVertex3f(56, 54, 0);
+		glVertex3f(56, 58, 0);		
+		glVertex3f(61, 58, 0);
 	glEnd();
 	
 	glFlush();
@@ -96,16 +137,73 @@ void KeyboardManagement(int key, int x, int y){
 	glutPostRedisplay();	
 }
 
-//void CreateMenu(){
-//	
-//}
-//
-//void MouseManagement(int button, int state, int x, int y){
-//	if(button == GLUT_RIGHT_BUTTON){
-//		if(state == GLUT_DOWN)
-//			CreateMenu();
-//	}
-//}
+void LabColor(int choice){
+	switch(choice){
+		case 0:
+			lR=1; lG=0; lB=0;
+			break;
+		case 1:
+			lR=0; lG=1; lB=0;
+			break;
+		case 2:
+			lR=0; lG=0; lB=1;
+			break;
+		case 3:
+			lR=0; lG=0; lB=0;
+			break;
+	}
+	
+	glutPostRedisplay();
+}
+
+void PlayerColor(int choice){
+	switch(choice){
+		case 0:
+			pR=1; pG=0; pB=0;
+			break;
+		case 1:
+			pR=0; pG=1; pB=0;
+			break;
+		case 2:
+			pR=0; pG=0; pB=1;
+			break;
+	}
+	
+	glutPostRedisplay();
+}
+
+void MainMenu(int choice){}
+
+void NewMenu(){
+	
+	int menu, submenu1, submenu2;
+	
+	submenu1=glutCreateMenu(LabColor);
+	glutAddMenuEntry("Red",0);
+    glutAddMenuEntry("Green",1);
+    glutAddMenuEntry("Blue",2);
+    glutAddMenuEntry("Black",3);
+    
+    submenu2 = glutCreateMenu(PlayerColor);
+    glutAddMenuEntry("Red",0);
+    glutAddMenuEntry("Green",1);
+    glutAddMenuEntry("Blue",2);
+	
+	menu=glutCreateMenu(MainMenu);
+	glutAddSubMenu("Labirynth color",submenu1);
+	glutAddSubMenu("Player color",submenu2);
+	
+	glutAttachMenu(GLUT_LEFT_BUTTON);
+}
+
+void MouseManagement(int button, int state, int x, int y){
+	if(button == GLUT_LEFT_BUTTON){
+		if(state == GLUT_DOWN)
+			NewMenu();
+	}
+	
+	glutPostRedisplay();
+}
 
 int main(void){
 	
@@ -117,7 +215,7 @@ int main(void){
 	Init();
 	glutDisplayFunc(Draw);
 	glutSpecialFunc(KeyboardManagement);
-//	glutMouseFunc(MouseManagement);
+	glutMouseFunc(MouseManagement);
 	glutMainLoop();
 }
 
